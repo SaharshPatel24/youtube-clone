3415992c606097b4ccad6ba35b2a451b5bc7166d
SaharshPatel24 <srpatel56@myseneca.ca>
2022-06-03 00:46:37 -0400

    Initial Commit

---
+++ pages/card.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import { VisibilitySharp } from '@mui/icons-material';
+
+const VideoCard = (props: any) => {
+    const Url = "https://www.googleapis.com/youtube/v3/videos?part=statistics&id=" + props.props.id.videoId + "&key=AIzaSyAxd5S2TAk2vhoQdhzSZqKDNTv_ok_-yos";
+
+    const { isLoading, error, data } = useQuery('repoData', () =>
+        fetch(Url).then(res =>
+            res.json().then(data => data.items),
+        )
+    )
+
+    if (isLoading) return <h1>'Loading...'</h1>;
+
+    if (error) return (<>An error has occurred: {error}</>);
+
+
+    return (
+        <Card raised sx={{ maxWidth: 345, maxHeight: 375, }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    width="200"
+                    height="200"
+                    image={props.props.snippet.thumbnails.high.url}
+                    alt="green iguana"
+                />
+                <CardContent sx={{ fontSize: 10 }}>
+                    <Typography gutterBottom variant="body1" component="div">
+                        {props.props.snippet.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {props.props.snippet.channelTitle}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions >
+                <Button size="small" sx={{ color: "gray" }}><VisibilitySharp ></VisibilitySharp>&nbsp;{data.viewCount}</Button>
+                <Button size="small" sx={{ color: "gray" }}><ChatBubbleIcon></ChatBubbleIcon>&nbsp;{data.commentCount}</Button>
+            </CardActions>
+
+        </Card >
+
+    );
+}
+
+export default VideoCard;
---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import VideoCard from './card';
+import { SimpleGrid } from '@chakra-ui/react';
+
+const Home = () => {
+  const [query, setQuery] = useState("");
+  const { isLoading, error, data } = useQuery('repoData', () =>
+    fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=%20' + query + '&key=AIzaSyAxd5S2TAk2vhoQdhzSZqKDNTv_ok_-yos').then(res =>
+      res.json().then(data => data.items),
+    )
+  )
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error
+
+  return (
+    <div className="home">
+      <div className="searchbar">
+        <input type="text" placeholder="Search" onChange={e => setQuery(e.target.value)}></input><button className="search">Search</button>
+      </div>
+      <SimpleGrid columns={[2, null, 4]} minChildWidth={345} spacing={4} style={{ width: '75%', margin: "5% 10% 5% 13%" }}>
+        {
+          data.map((data: any, i: any) => {
+            return <VideoCard props={data} key={i}></VideoCard>
+          })
+        }
+      </SimpleGrid>
+    </div>
+  )
+}
+
+export default Home
